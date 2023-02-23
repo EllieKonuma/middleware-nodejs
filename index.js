796e1e21d5d29cab951989f1671ec355bce85e4d
EllieKonuma <elliekonuma@gmail.com>
2023-02-23 03:40:34 -0300

    create purchase endpoint

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+const { urlencoded } = require("body-parser");
 const client = require("./client");
 
 // Setup API
 const port = 3000;
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(urlencoded({ extended: true }));
+app.use(express.json());
 
 // Define routes
 app.get("/products", async (_, res) => {
@@ -26,18 +27,29 @@ app.get("/users", async (_, res) => {
 
 app.get("/users/:userId", async (req, res) => {
   const { userId } = req.params;
-  const user = await client.getProductById(userId);
+  const user = await client.getUserById(userId);
   res.send(user);
+});
+
+app.post("/users/:userId/purchase", async (req, res) => {
+  const { userId } = req.params;
+  const productIds = req.body;
+
+  const { tax } = await client.getUserById(userId);
+  const products = await Promise.all(
+    productIds.map((id) => client.getProductById(id))
+  );
+
+  const productsPrice = products.reduce(
+    (priceSum, product) => priceSum + product.price,
+    0
+  );
+  const total = ((tax / 100) * productsPrice).toFixed(2);
+
+  res.send({ total: total });
 });
 
 // Start server
 app.listen(port, () => {
   console.log(`Servidor está executando na porta ${port}.`);
 });
-
-// app.get("/user/:userId/purchase", (req, res) => {
-//   // let conv = req.query.id.map((id) => getProduto(id)
-//   // ({ id: Number(id), price: Math.random() * 100 })
-//   // );
-//   res.send({ params: req.params, query: req.query });
-// });
