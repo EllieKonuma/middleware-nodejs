c694bc95799982159a7ffb6a5f88960d432f62d0
Ellie <75400838+EllieKonuma@users.noreply.github.com>
2023-02-23 15:07:34 -0300

    add controllers to routes (#1)
    * add controllers to routes
    
    * refactor controller name

---
+++ src/controller.js
@@ -0,0 +1,34 @@
+const client = require("./client");
+
+const getProducts = async () => await client.getProducts();
+
+const getProductById = async (productId) =>
+  await client.getProductById(productId);
+
+const getUsers = async () => await client.getUsers();
+
+const getUserById = async (userId) => await client.getProductById(userId);
+
+const getProductsPrice = async (userId, productIds) => {
+  const initialSum = 0;
+
+  const { tax } = await client.getUserById(userId);
+  const products = await Promise.all(
+    productIds.map((id) => client.getProductById(id))
+  );
+
+  const productsPrice = products.reduce(
+    (priceSum, product) => priceSum + product.price,
+    initialSum
+  );
+
+  return parseFloat((productsPrice * (tax / 100)).toFixed(2));
+};
+
+module.exports = {
+  getProducts,
+  getProductById,
+  getUsers,
+  getUserById,
+  getProductsPrice,
+};
